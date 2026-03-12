ae3a6f37f7fd53e4306855ae5f6054e941f9a4a5
CassiaEliza <cassiaelizarocha@gmail.com>
2026-03-12 09:56:34 -0300

    Fix base path for dev/prod modes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/pooltalent---gest-o-de-tempor-rios--2-/',
+      base: mode === 'production' ? '/pooltalent---gest-o-de-tempor-rios--2-/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
